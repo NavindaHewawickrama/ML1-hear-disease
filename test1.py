bc979a10060c1de49ef6420aaa058afd9e130a1c
Navinda Hewawickrama <hewawickraman@gmail.com>
2023-12-04 07:16:23 +0530

    Initial commit

---
+++ test1.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
+
+heart_data = pd.read_csv('C:/Users/hewaw/OneDrive/Desktop/ML research/python_test/heart_disease_data.csv')
+heart_data.info()
+
+
+#checking for missing values
+heart_data.isnull().sum()
+
+#statistical measures of the data
+heart_data.describe()
